f420e8d2cd75e6b5a5ee64d032219bce62ebf76b
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-04-26 13:04:58 +0300

    Implement Ninja Class| add getNinjaSound method

--- js/ninja.js
+++ js/ninja.js
@@ -1,1 +1,15 @@
 // todo list
+class Ninja{
+
+    constructor(name, age, weight, power){
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.power = power;
+    }
+    getNinjaSound(){
+        return "wohaa! I'm "+this.name;
+    }
+}
+let ninja1 =  new Ninja("Jon", 25, 78, 200);
+console.log(ninja1.getNinjaSound());
